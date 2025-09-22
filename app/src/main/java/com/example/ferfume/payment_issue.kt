7df9990f59c2d5f60768137f06bac065e103493f
Khangale Khangale <khangi@silversurge.co.za>
2025-09-22 23:52:35 +0200

    Fix missing ID in activity_payment_issue layout

--- app/src/main/java/com/example/ferfume/payment_issue.kt
+++ app/src/main/java/com/example/ferfume/payment_issue.kt
@@ -11,10 +11,6 @@ class payment_issue : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_payment_issue)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
